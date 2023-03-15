4c8dfc59ee5dc7095ea6e8dcde9ef0e136aa97fe
sweatshirt0 <sweatshirttt@proton.me>
2023-03-15 13:35:26 -0400

    change port num

--- server.js
+++ server.js
@@ -245,6 +245,6 @@ const server = http.createServer((req, res) => {
 
 });
 
-const port = process.env.port || 9000;
+const port = process.env.port || 8080;
 
 server.listen(port, console.log(`Server running on port ${port}`));
